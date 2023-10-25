0a9cc9a0a965ff3e847c848a7a3163c6d3bcb8d6
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2023-10-25 03:55:49 +0100

    changed navigation to become absolute to stay above next element

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,19 +1,21 @@
-<div class="shadow rounded-b-2xl">
-    <div class="max-w-7xl mx-auto flex justify-between items-center px-6 py-4">
-        <!-- Logo -->
-        <div>
-            <x-application-logo class="w-24" />
-        </div>
+<div class="relative">
+    <div class="absolute w-full top-0 left-0 z-50 bg-white shadow rounded-b-2xl">
+        <div class="max-w-7xl mx-auto flex justify-between items-center px-6 py-4">
+            <!-- Logo -->
+            <div>
+                <x-application-logo class="w-24" />
+            </div>
 
-        <!-- Menu -->
-        <div class="">
-            <ul class="flex">
-                <x-site.nav-link href="{{ route('site.home') }}">{{ __("Home") }}</x-site.nav-link>
-                <x-site.nav-link href="{{ route('site.about') }}">{{ __("About") }}</x-site.nav-link>
-                <x-site.nav-link href="{{ route('site.about') }}">{{ __("Products") }}</x-site.nav-link>
-                <x-site.nav-link href="{{ route('site.about') }}">{{ __("Services") }}</x-site.nav-link>
-                <x-site.nav-link href="{{ route('site.about') }}">{{ __("Cart") }}</x-site.nav-link>
-            </ul>
+            <!-- Menu -->
+            <div class="">
+                <ul class="flex">
+                    <x-site.nav-link href="{{ route('site.home') }}">{{ __("Home") }}</x-site.nav-link>
+                    <x-site.nav-link href="{{ route('site.about') }}">{{ __("About") }}</x-site.nav-link>
+                    <x-site.nav-link href="{{ route('site.about') }}">{{ __("Products") }}</x-site.nav-link>
+                    <x-site.nav-link href="{{ route('site.about') }}">{{ __("Services") }}</x-site.nav-link>
+                    <x-site.nav-link href="{{ route('site.about') }}">{{ __("Cart") }}</x-site.nav-link>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
