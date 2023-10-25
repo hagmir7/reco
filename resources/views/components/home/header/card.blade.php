4f08f4997b7b940f9da53a284bc0eedb4249cbd0
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2023-10-25 03:54:21 +0100

    created card component for home page.

---
+++ resources/views/components/home/header/card.blade.php
@@ -0,0 +1,14 @@
+<div class="card shadow-[-7px_5px_18px_2px_rgba(0,0,0,0.12)] rounded-lg hover:cursor-pointer h-full">
+    <div class="h-full flex flex-col justify-between gap-8">
+        <div class="flex justify-end">
+            {{ $image }}
+        </div>
+        <div class="flex flex-col gap-2">
+            <h3 class="text-3xl font-bold">{{ $title }}</h3>
+            <p>{{ $description }}</p>
+        </div>
+        <div class="flex justify-end items-end">
+            {{ $icon }}
+        </div>
+    </div>
+</div>
