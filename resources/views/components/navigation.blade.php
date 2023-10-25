51813e4734eed4b9cef1a12d94bd31aa2b5863a3
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2023-10-25 02:12:15 +0100

    updated navigation.blade.php

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,18 +1,18 @@
-<div class="shadow">
-    <div class="max-w-7xl mx-auto flex justify-between px-6 py-4">
+<div class="shadow rounded-b-2xl">
+    <div class="max-w-7xl mx-auto flex justify-between items-center px-6 py-4">
         <!-- Logo -->
         <div>
-            <x-application-logo />
+            <x-application-logo class="w-24" />
         </div>
 
         <!-- Menu -->
         <div class="">
             <ul class="flex gap-6">
-                <x-site.nav-link>{{ __("Home") }}</x-site.nav-link>
-                <x-site.nav-link>{{ __("About") }}</x-site.nav-link>
-                <x-site.nav-link>{{ __("Products") }}</x-site.nav-link>
-                <x-site.nav-link>{{ __("Services") }}</x-site.nav-link>
-                <x-site.nav-link>{{ __("Cart") }}</x-site.nav-link>
+                <x-site.nav-link href="{{ route('site.home') }}">{{ __("Home") }}</x-site.nav-link>
+                <x-site.nav-link href="{{ route('site.about') }}">{{ __("About") }}</x-site.nav-link>
+                <x-site.nav-link href="{{ route('site.about') }}">{{ __("Products") }}</x-site.nav-link>
+                <x-site.nav-link href="{{ route('site.about') }}">{{ __("Services") }}</x-site.nav-link>
+                <x-site.nav-link href="{{ route('site.about') }}">{{ __("Cart") }}</x-site.nav-link>
             </ul>
         </div>
     </div>
