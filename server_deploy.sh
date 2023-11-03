18ff02ea9351ec5132936013fb1dcc3d3a1f9466
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2023-11-03 16:21:02 +0100

    updated server_deploy.sh

--- server_deploy.sh
+++ server_deploy.sh
@@ -4,26 +4,26 @@ set -e
 echo "Deploying application ..."
 
 # Enter maintenance mode
-(php81 artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+(/usr/local/bin/ea-php81 artisan down) || true
     # Update codebase
-    git fetch origin deploy
-    git reset --hard origin/deploy
+    git fetch origin master
+    git reset --hard origin/master
 
     # Install dependencies based on lock file
-    php81 ~/composer.phar install --no-interaction --prefer-dist --optimize-autoloader
+    /usr/local/bin/ea-php81 ~/composer.phar install --no-interaction --prefer-dist --optimize-autoloader
 
     # Migrate database
-    php81 artisan migrate --force
+    /usr/local/bin/ea-php81 artisan migrate --force
 
     # Note: If you're using queue workers, this is the place to restart them.
     # ...
 
     # Clear cache
-    php81 artisan optimize
+    /usr/local/bin/ea-php81 artisan optimize
 
     # Reload PHP to update opcache
     # echo "" | sudo -S service php8.1-fpm reload
 # Exit maintenance mode
-php81 artisan up
+/usr/local/bin/ea-php81 artisan up
 
 echo "Application deployed!"
