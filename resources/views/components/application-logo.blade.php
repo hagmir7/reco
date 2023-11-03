f16664a90d1d070d499a579273e917f372df1865
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2023-11-03 14:21:37 +0100

    added home link to application-logo.blade.php

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
-<div>
+<a class="block" href="/">
     <img {{ $attributes->merge(['class' => 'dark:hidden']) }} src="{{ config('shopper.system.logo') }}" alt="{{ config('app.name') }}" />
     <img {{ $attributes->merge(['class' => 'hidden dark:inline']) }} src="{{ config('shopper.system.logo_white') }}" alt="{{ config('app.name') }}" />
-</div>
+</a>
