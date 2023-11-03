418317aceb66d34e7965a7b02519b305a3605ee4
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2023-11-03 14:24:34 +0100

    finished footer structure

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,37 @@
-<div>
-    <!-- Order your soul. Reduce your wants. - Augustine -->
+<div class="w-full mt-12 shadow-[0px_-10px_12px_0px_rgba(0,0,0,0.15)]">
+    <div class="max-w-7xl mx-auto grid grid-cols-12 gap-6 py-20">
+        <div class="col-span-full md:col-span-6 lg:col-span-3">
+            <x-application-logo class="w-36"/>
+        </div>
+        <div class="col-span-full md:col-span-6 lg:col-span-3">
+            <ul class="flex flex-col gap-4">
+                <li class="font-bold">
+                    <a href="{{ route('site.home') }}">{{ __("About") }}</a>
+                </li>
+                <li class="font-bold">
+                    <a href="{{ route('site.home') }}">{{ __("Products") }}</a>
+                </li>
+                <li class="font-bold">
+                    <a href="{{ route('site.home') }}">{{ __("Services") }}</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-span-full md:col-span-6 lg:col-span-3 flex items-center">
+            <div class="text-xl">Keep in touch</div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="85" height="24" viewBox="0 0 94 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" speechify-initial-font-family="ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, &quot;Noto Sans&quot;, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;, &quot;Noto Color Emoji&quot;" speechify-initial-font-size="16px">
+                <path stroke="none" d="M0 0h85v24H0z" fill="none" speechify-initial-font-family="ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, &quot;Noto Sans&quot;, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;, &quot;Noto Color Emoji&quot;" speechify-initial-font-size="16px"></path>
+                <path d="M5 12l84 0" speechify-initial-font-family="ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, &quot;Noto Sans&quot;, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;, &quot;Noto Color Emoji&quot;" speechify-initial-font-size="16px"></path>
+                <path d="M85 16l4 -4" speechify-initial-font-family="ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, &quot;Noto Sans&quot;, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;, &quot;Noto Color Emoji&quot;" speechify-initial-font-size="16px"></path>
+                <path d="M85 8l4 4" speechify-initial-font-family="ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, &quot;Noto Sans&quot;, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;, &quot;Noto Color Emoji&quot;" speechify-initial-font-size="16px"></path>
+            </svg>
+        </div>
+        <div class="flex col-span-full md:col-span-6 lg:col-span-3">
+            <ul class="flex items-center gap-2">
+                <li><img src="/assets/images/icons/fb.svg" alt="fb logo"></li>
+                <li><img src="/assets/images/icons/linkedin.svg" alt="linkedin logo"></li>
+                <li><img src="/assets/images/icons/instagram.svg" alt="instagram logo"></li>
+                <li><img src="/assets/images/icons/twitter.svg" alt="fb logo"></li>
+            </ul>
+        </div>
+    </div>
 </div>
