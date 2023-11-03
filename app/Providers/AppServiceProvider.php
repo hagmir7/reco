629e9e0b47df20c21361de6dfcc5852652c3200c
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2023-11-03 14:23:43 +0100

    added multiSearch Macro and changed Schema defaultStringLength in AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Closure;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,5 +26,16 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Builder::macro('multiSearch', function (array $fields, $string) {
+            return $this->when($string, function ($query) use ($fields, $string) {
+                $query->where(function ($query) use ($fields, $string) {
+                    foreach ($fields as $field) {
+                        $query->orWhere($field, 'like', '%' . $string . '%');
+                    }
+                });
+            });
+        });
+
+        Schema::defaultStringLength(191);
     }
 }
