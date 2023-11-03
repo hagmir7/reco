f6c7a78bc550498268dffe26133f5b10cbdd4bd1
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2023-11-03 14:24:56 +0100

    updated navigation

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,21 +1,61 @@
-<div class="relative">
-    <div class="absolute w-full top-0 left-0 z-50 bg-white shadow rounded-b-2xl">
+<div>
+    <div class="w-full z-50 bg-gray-900 shadow">
         <div class="max-w-7xl mx-auto flex justify-between items-center px-6 py-4">
             <!-- Logo -->
             <div>
-                <x-application-logo class="w-24" />
+                <x-application-logo class="absolute top-0 h-36 shadow-lg rounded-b-lg" />
             </div>
 
             <!-- Menu -->
             <div class="">
-                <ul class="flex">
+                <ul class="flex" x-data="{ openCart: false }">
                     <x-site.nav-link href="{{ route('site.home') }}">{{ __("Home") }}</x-site.nav-link>
                     <x-site.nav-link href="{{ route('site.about') }}">{{ __("About") }}</x-site.nav-link>
-                    <x-site.nav-link href="{{ route('site.about') }}">{{ __("Products") }}</x-site.nav-link>
-                    <x-site.nav-link href="{{ route('site.about') }}">{{ __("Services") }}</x-site.nav-link>
-                    <x-site.nav-link href="{{ route('site.about') }}">{{ __("Cart") }}</x-site.nav-link>
+                    <x-site.nav-link href="{{ route('site.products.index') }}">{{ __("Products") }}</x-site.nav-link>
+                    <x-site.nav-link href="{{ route('site.services') }}">{{ __("Services") }}</x-site.nav-link>
+                    <x-site.nav-link href="#"
+                                     x-data="cart"
+                                     x-on:keydown.escape.prevent.stop="close($refs.button)"
+                                     x-on:focusin.window="! $refs.panel.contains($event.target) && close()"
+                                     x-id="['dropdown-button']">
+
+                        <button x-ref="button"
+                            x-on:click="toggle()"
+                            :aria-expanded="open"
+                            :aria-controls="$id('dropdown-button')"
+                            type="button"
+                            class="flex items-center p-0">
+                            {{ __("Cart") }}
+
+                            <span class="absolute top-0 right-0 inline-flex items-center py-0.5 px-1.5 rounded-full text-xs font-medium transform -translate-y-1/4 translate-x-1/4 text-gray-900 bg-primary-500">
+                                {{ \Jackiedo\Cart\Facades\Cart::name('panier')->sumItemsQuantity() }}
+                            </span>
+                        </button>
+                        <div x-ref="panel"
+                             x-show="open"
+                             x-transition.origin.top.left
+                             x-on:click.outside="close($refs.button)"
+                             :id="$id('dropdown-button')"
+                             class="hs-dropdown-menu hs-dropdown-open:opacity-100 w-72 transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden z-10 transition-[margin,opacity] opacity-0 duration-300 mt-2 min-w-[15rem] bg-white shadow-md rounded-lg p-2 dark:bg-gray-800 dark:border dark:border-gray-700 dark:divide-gray-700" aria-labelledby="hs-dropdown-slideup-animation">
+                            <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
+                                Newsletter
+                            </a>
+                            <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
+                                Purchases
+                            </a>
+                            <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
+                                Downloads
+                            </a>
+                            <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
+                                Team Account
+                            </a>
+                        </div>
+                    </x-site.nav-link>
                 </ul>
+
             </div>
         </div>
     </div>
 </div>
+
+
--- resources/views/components/site/nav-link.blade.php
+++ resources/views/components/site/nav-link.blade.php
@@ -1,4 +1,4 @@
 @props(['class' => '', 'href' => '#'])
-<a href="{{ $href }}" @class(["font-semibold hover:text-primary-500 hover:underline px-4 py-4", $class , 'text-primary-500 underline' => request()->fullUrl() == $href]) {{ $attributes }}>
+<a href="{{ $href }}" @class(["relative font-semibold text-gray-100 hover:text-primary-500 hover:underline px-4 py-2", $class , 'text-primary-500 underline' => request()->fullUrl() == $href]) {{ $attributes }}>
     {{ $slot }}
 </a>
