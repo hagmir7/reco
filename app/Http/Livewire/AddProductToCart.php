6cc6b3d99adee2ee46737ac79a22933609cc29a8
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2023-11-03 14:47:33 +0100

    created AddProductToCart.php Livewire component

---
+++ app/Http/Livewire/AddProductToCart.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Product;
+use Jackiedo\Cart\Cart;
+use Livewire\Component;
+
+class AddProductToCart extends Component
+{
+    public Product $product;
+
+    public $quantity = 1;
+
+    public function mount(string $slug)
+    {
+        $this->product = Product::findBySlug($slug);
+    }
+
+    public function add()
+    {
+        $this->product->addToCart('panier', [
+            'quantity' => $this->quantity,
+            'price' => $this->product->price->amount,
+        ]);
+    }
+
+    public function render()
+    {
+        return view('livewire.add-product-to-cart');
+    }
+}
---
+++ resources/views/livewire/add-product-to-cart.blade.php
@@ -0,0 +1,5 @@
+<div>
+    {{-- The whole world belongs to you. --}}
+    <input wire:model.defer="quantity" class="px-4 py-2 rounded-md border border-gray-200">
+    <button class="btn btn-black" wire:click="add">Ajouter au panier</button>
+</div>
