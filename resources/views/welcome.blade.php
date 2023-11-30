49bdee03dee2ae69ccf60ecce4d78dee59e3ea03
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2023-11-30 13:33:56 +0100

    added animations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,20 +15,24 @@
 
         <div class="row-span-full col-span-full w-full top-0 left-0">
             <div class="max-w-7xl xl:max-w-8xl mx-auto px-8">
-                <div class="pt-48 pb-16 flex flex-col items-start gap-6">
-                    <div class="uppercase font-extrabold text-4xl lg:text-6xl text-white lg:text-black text-center lg:text-start max-w-[700px]">
+                <div class="pt-48 pb-16 flex flex-col items-center lg:items-start gap-6">
+                    <div data-aos="fade-up" class="uppercase font-extrabold text-4xl lg:text-6xl text-white lg:text-black text-center lg:text-start max-w-[700px]">
                         La
                         <x-special-text>Technologie Simplifiée</x-special-text>
                         pour rendre votre maison plus intelligente.
                     </div>
 
-                    <a class="btn btn-black px-14 font-bold bubble-animation" href="{{ route('site.products.index') }}">{{ __("Voir tous les produits") }}</a>
+                    <div data-aos="fade-up" data-aos-delay="600" data-aos-offset="0" >
+                        <a class="btn btn-black px-14 font-bold bubble-animation" href="{{ route('site.products.index') }}">
+                            {{ __("Voir tous les produits") }}
+                        </a>
+                    </div>
                 </div>
 
                 <div class="py-12">
                     <div class="max-w-5xl mx-auto grid md:grid-cols-3 gap-6 lg:gap-12">
                         <div class="">
-                            <x-home.header.card :title="$home_categories[0]?->name" href="{{ route('site.products.index') }}?category={{ $home_categories[0]?->slug }}">
+                            <x-home.header.card data-aos="fade-up" data-aos-delay="100" :title="$home_categories[0]?->name" href="{{ route('site.products.index') }}?category={{ $home_categories[0]?->slug }}">
                                 <x-slot name="image">
                                     <img data-src="{{ $home_categories[0]?->getFirstMediaUrl(config('shopper.system.storage.disks.uploads')) }}" class="w-48 lazy" alt="sécurité image">
                                 </x-slot>
@@ -49,7 +53,7 @@
                         </div>
 
                         <div class="">
-                            <x-home.header.card :title="$home_categories[1]?->name" href="{{ route('site.products.index') }}?category={{ $home_categories[1]?->slug }}">
+                            <x-home.header.card data-aos="fade-up" data-aos-delay="200" :title="$home_categories[1]?->name" href="{{ route('site.products.index') }}?category={{ $home_categories[1]?->slug }}">
                                 <x-slot name="image">
                                     <img data-src="{{ $home_categories[1]?->getFirstMediaUrl(config('shopper.system.storage.disks.uploads')) }}" class="w-48 lazy" alt="sécurité image">
                                 </x-slot>
@@ -71,7 +75,7 @@
 
                         <div class="">
                             <div class="flex flex-col gap-6 lg:gap-8">
-                                <div class="card py-4 px-6 shadow-[-7px_5px_18px_2px_rgba(0,0,0,0.12)] rounded-lg hover:cursor-pointer h-full">
+                                <div data-aos="fade-up" data-aos-delay="300" class="card py-4 px-6 shadow-[-7px_5px_18px_2px_rgba(0,0,0,0.12)] rounded-lg hover:cursor-pointer h-full">
                                     <a href="{{ route('site.products.index') }}?category={{ $home_categories[2]?->slug }}" class="h-full flex items-center justify-between gap-8">
                                         <div class="flex flex-col gap-2">
                                             <h3 class="text-3xl font-bold">{{ $home_categories[2]?->name }}</h3>
@@ -88,7 +92,7 @@
                                     </a>
                                 </div>
 
-                                <div class="card py-4 px-6 shadow-[-7px_5px_18px_2px_rgba(0,0,0,0.12)] rounded-lg hover:cursor-pointer h-full">
+                                <div data-aos="fade-up" data-aos-delay="300" class="card py-4 px-6 shadow-[-7px_5px_18px_2px_rgba(0,0,0,0.12)] rounded-lg hover:cursor-pointer h-full">
                                     <a href="{{ route('site.products.index') }}?category={{ $home_categories[3]?->slug }}" class="h-full flex items-center justify-between gap-8">
                                         <div class="flex flex-col gap-2">
                                             <h3 class="text-3xl font-bold">{{ $home_categories[3]?->name }}</h3>
@@ -113,17 +117,17 @@
     </div>
 
     <div class="max-w-7xl xl:max-w-8xl mx-auto px-8 flex flex-col items-center py-10 space-y-10">
-        <h1 class="text-4xl lg:text-5xl font-bold">{{ __("Our brands") }}</h1>
-        <div class="w-full lg:grid-cols-4 gap-6 justify-items-center">
+        <h1 data-aos="fade-up" class="text-4xl lg:text-5xl font-bold">{{ __("Our brands") }}</h1>
+        <div data-aos="fade-up" class="w-full lg:grid-cols-4 gap-6 justify-items-center">
             <x-brands-carousel :brands="$brands" />
         </div>
-        <a class="btn bg-black text-white px-12" href="{{ route('site.brands') }}">{{ __("Voir plus") }}</a>
+        <a data-aos="fade-up" class="btn bg-black text-white px-12" href="{{ route('site.brands') }}">{{ __("Voir plus") }}</a>
     </div>
 
     <div class="max-w-6xl mx-auto px-8 flex flex-col items-center py-10 space-y-20">
 
         <div class="grid grid-cols-12 grid-rows-12 lg:grid-rows-1">
-            <div class="rounded-lg p-8 md:p-16 pt-28 md:pt-32 lg:pt-16 bg-[#FFEE75] col-span-full lg:col-[1_/_span_9] row-[4_/_span_9] lg:row-start-1 py-20">
+            <div data-aos="zoom-in-right" class="rounded-lg p-8 md:p-16 pt-28 md:pt-32 lg:pt-16 bg-[#FFEE75] col-span-full lg:col-[1_/_span_9] row-[4_/_span_9] lg:row-start-1 py-20">
                 <div class="lg:w-[90%] flex flex-col items-start gap-6">
                     <h2 class="text-3xl lg:text-5xl font-bold leading-tight">Éclairage, Ombrage et Confort</h2>
 
@@ -144,14 +148,14 @@
                     </a>
                 </div>
             </div>
-            <div class="col-span-full lg:col-span-4 col-start-1 lg:col-start-9 row-[1_/_span_4] lg:row-[1_/_span_full] flex justify-center items-center">
+            <div data-aos="zoom-in-right" data-aos-delay="800" class="col-span-full lg:col-span-4 col-start-1 lg:col-start-9 row-[1_/_span_4] lg:row-[1_/_span_full] flex justify-center items-center">
                 <img class="max-w-lg w-full rounded-lg lazy" data-src="/assets/images/home/service-1.png" alt="">
             </div>
         </div>
 
         <div class="grid grid-cols-12 grid-rows-12 lg:grid-rows-1">
 
-            <div class="rounded-lg p-8 md:p-16 pt-28 md:pt-32 lg:pt-16 col-span-full lg:col-[4_/_span_9] row-[4_/_span_9] lg:row-start-1 flex justify-end bg-[#FFD873] py-20">
+            <div data-aos="zoom-in-left" class="rounded-lg p-8 md:p-16 pt-28 md:pt-32 lg:pt-16 col-span-full lg:col-[4_/_span_9] row-[4_/_span_9] lg:row-start-1 flex justify-end bg-[#FFD873] py-20">
                 <div class="lg:w-[90%] flex flex-col items-start gap-6">
                     <h2 class="text-3xl lg:text-5xl font-bold leading-tight">Divertissement</h2>
 
@@ -173,14 +177,14 @@
                 </div>
             </div>
 
-            <div class="col-[1_/_span_full] lg:col-[1_/_span_4] row-[1_/_span_4] lg:row-start-1 flex justify-center items-center">
+            <div data-aos="zoom-in-left" data-aos-delay="800" class="col-[1_/_span_full] lg:col-[1_/_span_4] row-[1_/_span_4] lg:row-start-1 flex justify-center items-center">
                 <img class="max-w-lg w-full rounded-lg lazy" data-src="/assets/images/home/service-2.png" alt="">
             </div>
 
         </div>
 
         <div class="grid grid-cols-12 grid-rows-12 lg:grid-rows-1">
-            <div class="rounded-lg p-8 md:p-16 pt-28 md:pt-32 lg:pt-16 bg-[#FFBF75] col-span-full lg:col-[1_/_span_9] row-[4_/_span_9] lg:row-start-1 py-20">
+            <div data-aos="zoom-in-right" class="rounded-lg p-8 md:p-16 pt-28 md:pt-32 lg:pt-16 bg-[#FFBF75] col-span-full lg:col-[1_/_span_9] row-[4_/_span_9] lg:row-start-1 py-20">
                 <div class="lg:w-[90%] flex flex-col items-start gap-6">
                     <h2 class="text-3xl lg:text-5xl font-bold leading-tight">Sécurité & Contrôle d'Accès</h2>
 
@@ -201,17 +205,17 @@
                     </a>
                 </div>
             </div>
-            <div class="col-span-full lg:col-span-4 col-start-1 lg:col-start-9 row-[1_/_span_4] lg:row-[1_/_span_full] flex justify-center items-center">
+            <div data-aos="zoom-in-right" data-aos-delay="800" class="col-span-full lg:col-span-4 col-start-1 lg:col-start-9 row-[1_/_span_4] lg:row-[1_/_span_full] flex justify-center items-center">
                 <img class="max-w-lg w-full rounded-lg lazy" data-src="/assets/images/home/service-3.png" alt="">
             </div>
         </div>
     </div>
 
     <div class="max-w-6xl mx-auto px-8 flex flex-col items-center">
-        <a href="{{ route('site.products.index') }}" class="btn btn-black px-14 font-bold">{{ __("Voir tous les produits") }}</a>
+        <a data-aos="fade-up" href="{{ route('site.products.index') }}" class="btn btn-black px-14 font-bold">{{ __("Voir tous les produits") }}</a>
     </div>
 
-    <div class="shadow-topBottom py-10 my-20">
+    <div data-aos="fade-up" class="shadow-topBottom py-10 my-20">
         <div class="max-w-6xl mx-auto flex flex-col items-center space-y-10">
             <h2 class="text-4xl lg:text-5xl font-bold">{{ __("Best selling") }}</h2>
 
@@ -231,16 +235,16 @@
 
         <div class="grid grid-cols-12 grid-rows-12 lg:grid-rows-1">
             <div class="col-[1_/_span_full] lg:col-[1_/_span_7] row-[1_/_span_8] lg:row-[1_/_span_full] flex justify-center items-center">
-                <img class="w-full rounded-lg lazy" data-src="/assets/images/home/service-4.png" alt="">
+                <img data-aos="zoom-in-right" class="w-full rounded-lg lazy" data-src="/assets/images/home/service-4.png" alt="">
             </div>
 
             <div class="col-[1_/_span_full] lg:col-[7_/_span_6] row-[8_/_span_4] lg:row-[1_/_span_full] pl-8 flex items-center">
                 <div class="flex flex-col items-center lg:items-start gap-6">
-                    <h2 class="uppercase text-3xl md:text-4xl md:w-[80%] lg:text-5xl text-center lg:text-left font-bold">
+                    <h2 data-aos="zoom-in-right" class="uppercase text-3xl md:text-4xl md:w-[80%] lg:text-5xl text-center lg:text-left font-bold">
                         {{ __("Faites passer votre maison au niveau supérieur") }}
                     </h2>
 
-                    <a href="{{ route('site.contact') }}" class="btn btn-black rounded-full inline-flex items-center gap-4">
+                    <a data-aos="zoom-in-right" href="{{ route('site.contact') }}" class="btn btn-black rounded-full inline-flex items-center gap-4">
                         {{ __("Contacter un technicien") }}
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-narrow-right" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
