8d5cf829dd63347470dcb7dc850af4ffb490dbd7
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2023-11-30 12:18:52 +0100

    installed silviolleite/laravelpwa

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -18,6 +18,7 @@
     @wireUiScripts
     {{-- app.js should always be after wireUiScripts because it contains alpinejs and is required to be after it--}}
     @vite('resources/js/app.js')
+    @laravelPWA
 </head>
 <body>
     <x-dialog />
