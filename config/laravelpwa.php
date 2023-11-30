d10125f65fed5cdd60306764b9b76946ea92fc4d
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2023-11-30 12:31:43 +0100

    configured pwa in the app

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -4,7 +4,7 @@ return [
     'name' => 'LaravelPWA',
     'manifest' => [
         'name' => env('APP_NAME', 'My PWA App'),
-        'short_name' => 'PWA',
+        'short_name' => 'Diasmart',
         'start_url' => '/',
         'background_color' => '#ffffff',
         'theme_color' => '#000000',
@@ -13,64 +13,72 @@ return [
         'status_bar'=> 'black',
         'icons' => [
             '72x72' => [
-                'path' => '/images/icons/icon-72x72.png',
+                'path' => '/assets/pwa/icons/icon-72x72.png',
                 'purpose' => 'any'
             ],
             '96x96' => [
-                'path' => '/images/icons/icon-96x96.png',
+                'path' => '/assets/pwa/icons/icon-96x96.png',
                 'purpose' => 'any'
             ],
             '128x128' => [
-                'path' => '/images/icons/icon-128x128.png',
+                'path' => '/assets/pwa/icons/icon-128x128.png',
                 'purpose' => 'any'
             ],
             '144x144' => [
-                'path' => '/images/icons/icon-144x144.png',
+                'path' => '/assets/pwa/icons/icon-144x144.png',
                 'purpose' => 'any'
             ],
             '152x152' => [
-                'path' => '/images/icons/icon-152x152.png',
+                'path' => '/assets/pwa/icons/icon-152x152.png',
                 'purpose' => 'any'
             ],
             '192x192' => [
-                'path' => '/images/icons/icon-192x192.png',
+                'path' => '/assets/pwa/icons/icon-192x192.png',
                 'purpose' => 'any'
             ],
-            '384x384' => [
-                'path' => '/images/icons/icon-384x384.png',
+            '256x256' => [
+                'path' => '/assets/pwa/icons/icon-256x256.png',
                 'purpose' => 'any'
             ],
             '512x512' => [
-                'path' => '/images/icons/icon-512x512.png',
+                'path' => '/assets/pwa/icons/icon-512x512.png',
+                'purpose' => 'any'
+            ],
+            '1024x1024' => [
+                'path' => '/assets/pwa/icons/icon-1024x1024.png',
                 'purpose' => 'any'
             ],
         ],
         'splash' => [
-            '640x1136' => '/images/icons/splash-640x1136.png',
-            '750x1334' => '/images/icons/splash-750x1334.png',
-            '828x1792' => '/images/icons/splash-828x1792.png',
-            '1125x2436' => '/images/icons/splash-1125x2436.png',
-            '1242x2208' => '/images/icons/splash-1242x2208.png',
-            '1242x2688' => '/images/icons/splash-1242x2688.png',
-            '1536x2048' => '/images/icons/splash-1536x2048.png',
-            '1668x2224' => '/images/icons/splash-1668x2224.png',
-            '1668x2388' => '/images/icons/splash-1668x2388.png',
-            '2048x2732' => '/images/icons/splash-2048x2732.png',
+            '640x1136' => '/assets/pwa/splashes/splash-640x1136.png',
+            '750x1334' => '/assets/pwa/splashes/splash-750x1334.png',
+            '828x1792' => '/assets/pwa/splashes/splash-828x1792.png',
+            '1125x2436' => '/assets/pwa/splashes/splash-1125x2436.png',
+            '1242x2208' => '/assets/pwa/splashes/splash-1242x2208.png',
+            '1242x2688' => '/assets/pwa/splashes/splash-1242x2688.png',
+            '1536x2048' => '/assets/pwa/splashes/splash-1536x2048.png',
+            '1668x2224' => '/assets/pwa/splashes/splash-1668x2224.png',
+            '1668x2388' => '/assets/pwa/splashes/splash-1668x2388.png',
+            '2048x2732' => '/assets/pwa/splashes/splash-2048x2732.png',
         ],
         'shortcuts' => [
             [
-                'name' => 'Shortcut Link 1',
-                'description' => 'Shortcut Link 1 Description',
-                'url' => '/shortcutlink1',
+                'name' => 'Accueil',
+                'description' => "Page d'accueil",
+                'url' => '/',
                 'icons' => [
-                    "src" => "/images/icons/icon-72x72.png",
+                    "src" => "/assets/pwa/icons/icon-72x72.png",
                     "purpose" => "any"
                 ]
             ],
             [
-                'name' => 'Shortcut Link 2',
-                'description' => 'Shortcut Link 2 Description',
-                'url' => '/shortcutlink2'
+                'name' => 'Contact',
+                'description' => 'Contacter un technicien',
+                'url' => '/contact',
+                'icons' => [
+                    "src" => "/assets/pwa/icons/icon-72x72.png",
+                    "purpose" => "any"
+                ],
             ]
         ],
         'custom' => []
